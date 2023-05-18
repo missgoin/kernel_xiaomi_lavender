89f7a5420e3966eaf7f734ed76be14816706bd17
missgoin <90597569+missgoin@users.noreply.github.com>
2023-05-18 17:32:47 +0700

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1525,6 +1525,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1538,6 +1543,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+	
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
